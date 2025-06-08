390733bb0d5da476923cb41c2bc97723c9c3ddb0
nikunjjakhotia <nikunjjakhotia@gmail.com>
2025-06-08 08:13:34 -0400

    Create Day01Assignments.py

---
+++ Assignments/Day01/Day01Assignments.py
@@ -0,0 +1,11 @@
+#Day 01 Assignment
+"""Write a Python script that does the following:
+
+Prints your name
+Prints your age (as a number)
+Prints your favorite hobby
+Uses at least one escape sequence and one comment"""
+
+print ("Hello Folks, My name is : \t Nikunj Jakhotia.")
+print ("I am ",34," years old.")
+print("My hobbies are : Swimming and Coding.")
